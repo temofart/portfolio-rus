be103c3d04f4a17bf17212d5b52e0cd8e446e3ec
temofart <18art12@gmail.com>
2016-08-08 18:13:54 +0300

    original

---
+++ js/scroll.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $("a").click(function () {
+        var elementClick = $(this).attr("href")
+        var destination = $(elementClick).offset().top;
+        jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination - 0}, 800);
+        return false;
+    });
+});
